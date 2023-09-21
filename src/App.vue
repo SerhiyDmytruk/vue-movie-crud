f55d6f70b98962c83deee209426c27658c998119
Dmytruk Serhii <dmitruksl92@gmail.com>
2023-09-21 20:52:33 +0300

    Start workin on new form

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,40 @@ import { StarIcon } from "@heroicons/vue/24/solid";
 import { items } from "./movies.json";
 const movies = ref(items);
 
-function ratingUpdate(movieIndex, star){
-  movies.value[movieIndex].rating = star;
+function updateRating(movieIndex, rating) {
+  movies.value[movieIndex].rating = rating;
 }
-
 </script>
 
 <template>
   <div class="app">
     <div class="movie-list">
-      <div class="movie-item" v-for="(movie, movieIndex) in movies" :key="movie.id">
+      <div
+        class="movie-item"
+        v-for="(movie, movieIndex) in movies"
+        :key="movie.id"
+      >
         <div class="movie-item-image-wrapper">
-          <img :src="movie.image" class="movie-item-image" alt="" />
-
-          <div class="absolute top-5 right-5">
-              <StarIcon
-               class="h-10 w-10  "
-               :class="[movie.rating ? 'text-yellow-500' : 'text-gray-500']" />
-
-              <span class="movie-rating-mark">
-                <span v-if="movie.rating">{{ movie.rating }}</span> 
-                <span v-else class="text-yellow-500">-</span>
+          <div class="movie-item-star-wrapper">
+            <StarIcon
+              id="rating"
+              class="movie-item-star-rating-icon"
+              :class="[movie.rating ? 'text-yellow-500' : 'text-gray-500']"
+            />
+            <div class="movie-item-star-content-wrapper">
+              <span
+                v-if="movie.rating"
+                id="rating-stars"
+                class="movie-item-star-content-rating-rated"
+              >
+                {{ movie.rating }}
               </span>
+              <span v-else class="movie-item-star-content-rating-not-rated">
+                -
+              </span>
+            </div>
           </div>
+          <img :src="movie.image" class="movie-item-image" alt="" />
         </div>
 
         <div class="movie-item-content-wrapper">
@@ -49,27 +60,20 @@ function ratingUpdate(movieIndex, star){
               Rating: ({{ movie.rating }}/5)
             </span>
 
-            <span class="movie-item-rating-items">
-              
-
-              <span class="movie-item-rating-wrapper-default absolute  z-0">
-
-                <button
-                  v-for="star in 5"
-                  :key="star"
-                  class="movie-item-star-icon-button"
-                  :class="[
-                    star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
-                  ]"
-                  :disabled="star === movie.rating"
-                  @click="ratingUpdate(movieIndex, star)"
-                  >
-
-                  <StarIcon class="movie-item-star-icon" />
-                </button>
-                
-              </span>
-            </span>
+            <div class="movie-item-star-icon-wrapper">
+              <button
+                v-for="star in 5"
+                :key="star"
+                class="movie-item-star-icon-button"
+                :class="[
+                  star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
+                ]"
+                :disabled="star === movie.rating"
+                @click="updateRating(movieIndex, star)"
+              >
+                <StarIcon class="movie-item-star-icon" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
